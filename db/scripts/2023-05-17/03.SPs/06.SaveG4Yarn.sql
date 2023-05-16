47ed47adf54e623cba3b0f76d19cf22ed09486ae
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-16 20:26:44 +0700

    Update 2023-05-16 #6   - Add SP SaveG4Yarn

---
+++ db/scripts/2023-05-17/03.SPs/06.SaveG4Yarn.sql
@@ -0,0 +1,172 @@
+/****** Object:  StoredProcedure [dbo].[SaveG4Yarn]    Script Date: 11/26/2022 1:17:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SaveG4Yarn
+-- [== History ==]
+-- <2022-08-20> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- 
+-- =============================================
+CREATE PROCEDURE [dbo].[SaveG4Yarn] (
+  @EntryDate datetime
+, @ItemYarn nvarchar(30)
+, @PalletNo nvarchar(30)
+, @YarnType nvarchar(30)
+, @WeightQty decimal(16, 3)
+, @LotNo nvarchar(30)
+, @TraceNo nvarchar(30)
+, @KgPerCH decimal(16, 3)
+, @ConeCH decimal(16, 3)
+, @PalletType nvarchar(30)
+, @Item400 nvarchar(30)
+, @Unit nvarchar(20)
+, @RemainQty decimal(16, 3)
+, @ReceiveDate datetime
+, @ReceiveBy int
+, @UpdateDate datetime
+, @MovementDate datetime
+, @Verify bit
+, @Packing bit
+, @Clean bit
+, @Tearing bit
+, @Falldown bit
+, @Certification bit
+, @Invoice bit
+, @IdentifyArea bit
+, @AmountPallet bit
+, @Other nvarchar(1000)
+, @Action nvarchar(1000)
+, @FinishFlag bit
+, @DeleteFlag bit
+, @PkId int = NULL out
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF EXISTS (SELECT TOP 1 * FROM G4Yarn WHERE PkId = @PkId)
+        BEGIN
+            UPDATE G4Yarn 
+               SET EntryDate = @EntryDate
+                 , ItemYarn = @ItemYarn
+                 , PalletNo = @PalletNo
+                 , YarnType = @YarnType
+                 , WeightQty = @WeightQty
+                 , LotNo = @LotNo
+                 , TraceNo = @TraceNo
+                 , KgPerCH = @KgPerCH
+                 , ConeCH = @ConeCH
+                 , PalletType = @PalletType
+                 , Item400 = @Item400
+                 , Unit = @Unit
+                 , RemainQty = @RemainQty
+                 , ReceiveDate = @ReceiveDate
+                 , ReceiveBy = @ReceiveBy
+                 , UpdateDate = @UpdateDate
+                 , MovementDate = @MovementDate
+                 , Verify = @Verify
+                 , Packing = @Packing
+                 , Clean = @Clean
+                 , Tearing = @Tearing
+                 , Falldown = @Falldown
+                 , Certification = @Certification
+                 , Invoice = @Invoice
+                 , IdentifyArea = @IdentifyArea
+                 , AmountPallet = @AmountPallet
+                 , Other = @Other
+                 , [Action] = @Action
+                 , FinishFlag = @FinishFlag
+                 , DeleteFlag = @DeleteFlag
+             WHERE PkId = @PkId
+        END
+        ELSE
+        BEGIN
+			INSERT INTO G4Yarn
+		    (
+                  EntryDate
+                , ItemYarn
+                , PalletNo
+                , YarnType
+                , WeightQty
+                , LotNo
+                , TraceNo
+                , KgPerCH
+                , ConeCH
+                , PalletType
+                , Item400
+                , Unit
+                , RemainQty
+                , ReceiveDate
+                , ReceiveBy
+                , UpdateDate
+                , MovementDate
+                , Verify
+                , Packing
+                , Clean
+                , Tearing
+                , Falldown
+                , Certification
+                , Invoice
+                , IdentifyArea
+                , AmountPallet
+                , Other
+                , [Action]
+                , FinishFlag
+                , DeleteFlag
+			)
+			VALUES
+			(
+                  @EntryDate
+                , @ItemYarn
+                , @PalletNo
+                , @YarnType
+                , @WeightQty
+                , @LotNo
+                , @TraceNo
+                , @KgPerCH
+                , @ConeCH
+                , @PalletType
+                , @Item400
+                , @Unit
+                , @RemainQty
+                , @ReceiveDate
+                , @ReceiveBy
+                , @UpdateDate
+                , @MovementDate
+                , @Verify
+                , @Packing
+                , @Clean
+                , @Tearing
+                , @Falldown
+                , @Certification
+                , @Invoice
+                , @IdentifyArea
+                , @AmountPallet
+                , @Other
+                , @Action
+                , @FinishFlag
+                , @DeleteFlag
+			);
+
+			SET @PkId = @@IDENTITY;
+        END
+
+        -- Update Error Status/Message
+        SET @errNum = 0;
+        SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
