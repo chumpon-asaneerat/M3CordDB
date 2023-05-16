1bba43fcb9542d0c57ce3d1a6269da728d579781
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-16 20:40:22 +0700

    Update 2023-05-16 #7   - Add VIEW G4YarnView   - Add SP GetG4Yarns

---
+++ db/scripts/2023-05-17/03.SPs/07.GetG4Yarns.sql
@@ -0,0 +1,31 @@
+/****** Object:  StoredProcedure [dbo].[GetG4Yarns]    Script Date: 11/27/2022 9:58:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetG4Yarns
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetG4Yarns NULL
+-- =============================================
+CREATE PROCEDURE [dbo].[GetG4Yarns]
+(
+  @traceNo nvarchar(30) = NULL
+)
+AS
+BEGIN
+	SELECT *
+	  FROM G4YarnView
+	 WHERE UPPER(LTRIM(RTRIM(TraceNo))) = UPPER(LTRIM(RTRIM(COALESCE(@traceNo, TraceNo))))
+       AND (DeleteFlag IS NULL OR DeleteFlag = 0)
+       AND (FinishFlag IS NULL OR FinishFlag = 0)
+END
+
+GO
