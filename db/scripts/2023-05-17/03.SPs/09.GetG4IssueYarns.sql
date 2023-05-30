e680e67fea42ae9e63b273584c2ca385a3dd8bbd
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-30 09:44:03 +0700

    Update 2023-05-30 #2 - Add SP GetG4IssueYarns

---
+++ db/scripts/2023-05-17/03.SPs/09.GetG4IssueYarns.sql
@@ -0,0 +1,58 @@
+/****** Object:  StoredProcedure [dbo].[GetG4IssueYarns]    Script Date: 11/27/2022 9:58:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetG4IssueYarns
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetG4IssueYarns NULL
+-- =============================================
+CREATE PROCEDURE [dbo].[GetG4IssueYarns]
+(
+  @RequestNo nvarchar(30) = NULL
+)
+AS
+BEGIN
+    SELECT I.PkId
+         , I.RequestNo
+         , I.IssueDate
+         , I.IssueBy
+         , I.TraceNo
+         , I.PalletNo
+         , I.WeightQty
+         , I.ConeCH
+         , I.PalletType
+         , I.DeleteFlag
+         , I.[Remark]
+         , Y.ItemYarn
+         , Y.LotNo
+         , Y.YarnType
+         , Y.Item400
+         , Y.EntryDate
+         , Y.Packing
+         , Y.Clean
+         , Y.Tearing
+         , Y.Falldown 
+         , Y.Falldown
+         , Y.Certification
+         , Y.Invoice
+         , Y.IdentifyArea
+         , Y.AmountPallet
+         , Y.Other
+      FROM G4IssueYarn I, G4Yarn Y
+     WHERE UPPER(LTRIM(RTRIM(I.RequestNo))) = UPPER(LTRIM(RTRIM(COALESCE(@RequestNo, I.RequestNo))))
+       AND I.PalletNo = Y.PalletNo
+       AND (I.DeleteFlag IS NULL OR I.DeleteFlag = 0)
+     ORDER BY I.IssueDate , I.PalletNo;
+
+END
+
+GO
