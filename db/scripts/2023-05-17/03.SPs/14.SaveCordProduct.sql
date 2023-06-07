f662fb8f9d92e2b3375604731ace3e6e5fb297c1
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-07 09:44:31 +0700

    Update 2023-06-07 #4 - Add SP SaveCordProduct

---
+++ db/scripts/2023-05-17/03.SPs/14.SaveCordProduct.sql
@@ -0,0 +1,92 @@
+/****** Object:  StoredProcedure [dbo].[SaveCordProduct]    Script Date: 11/26/2022 1:17:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SaveCordProduct
+-- [== History ==]
+-- <2022-08-20> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- 
+-- =============================================
+CREATE PROCEDURE [dbo].[SaveCordProduct] (
+  @ProductLotNo nvarchar(30)
+, @CustomerCode nvarchar(30)
+, @CustomerName nvarchar(150)
+, @ItemYarn nvarchar(30)
+, @YarnType nvarchar(30)
+, @Item400 nvarchar(30)
+, @TargetQty decimal(16, 3)
+, @ActualQty decimal(16, 3)
+, @FinishFlag bit
+, @DeleteFlag bit
+, @CordProductPkId int = NULL out
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF EXISTS (SELECT TOP 1 * FROM CordProduct WHERE CordProductPkId = @CordProductPkId)
+        BEGIN
+            UPDATE CordProduct 
+               SET ProductLotNo = @ProductLotNo
+                 , CustomerCode = @CustomerCode
+                 , CustomerName = @CustomerName
+                 , ItemYarn = @ItemYarn
+                 , YarnType = @YarnType
+                 , Item400 = @Item400
+                 , TargetQty = @TargetQty
+                 , ActualQty = @ActualQty
+                 , FinishFlag = @FinishFlag
+                 , DeleteFlag = @DeleteFlag
+             WHERE @CordProductPkId = @@CordProductPkId
+        END
+        ELSE
+        BEGIN
+			INSERT INTO CordProduct
+		    (
+                  ProductLotNo
+                , CustomerCode
+                , CustomerName
+                , ItemYarn
+                , YarnType
+                , Item400
+                , TargetQty
+                , ActualQty
+                , FinishFlag
+                , DeleteFlag
+			)
+			VALUES
+			(
+                  @ProductLotNo
+                , @CustomerCode
+                , @CustomerName
+                , @ItemYarn
+                , @YarnType
+                , @Item400
+                , @TargetQty
+                , @ActualQty
+                , @FinishFlag
+                , @DeleteFlag
+			);
+
+			SET @CordProductPkId = @@IDENTITY;
+        END
+
+        -- Update Error Status/Message
+        SET @errNum = 0;
+        SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
