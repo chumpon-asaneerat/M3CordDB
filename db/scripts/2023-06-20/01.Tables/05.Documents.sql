da9854024e7c35fe7c80420d5c01a00337eac059
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-30 08:42:29 +0700

    Update 2023-06-30 #3 - Try to Implements Document Layout.

---
+++ db/scripts/2023-06-20/01.Tables/05.Documents.sql
@@ -0,0 +1,23 @@
+-- THIS SHOULD BE CLASS AND SAVE TO LOCAL FOLDER
+/*
+CREATE TABLE DocumentLayout(
+	LayoutId int IDENTITY(1,1) NOT NULL,
+    LayoutCode nvarchar(20) NOT NULL,
+    LayoutName nvarchar(100) NOT NULL,
+CONSTRAINT PK_DocumentLayout PRIMARY KEY (LayoutId ASC)
+)
+
+GO
+
+CREATE TABLE DocumentLayoutItem(
+	LayoutItemId int IDENTITY(1,1) NOT NULL,
+    LayoutCode nvarchar(20) NOT NULL,
+    RowNo int NOT NULL DEFAULT 0,
+    ColNo int NOT NULL DEFAULT 0,
+    RowSpan int NOT NULL DEFAULT 1,
+    ColSpan int NOT NULL DEFAULT 1,
+CONSTRAINT PK_DocumentLayoutItem PRIMARY KEY (LayoutItemId ASC)
+)
+
+GO
+*/
