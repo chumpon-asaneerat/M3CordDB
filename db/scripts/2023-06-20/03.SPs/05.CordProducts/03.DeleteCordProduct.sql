fff1a9b81181fa50ec56509b66a743d49d01a43e
chumpon-asaneerat <chumponsenate@gmail.com>
2023-07-05 05:31:40 +0700

    Update 2023-07-05 #4 - Add SP DeleteCordProduct

---
+++ db/scripts/2023-06-20/03.SPs/05.CordProducts/03.DeleteCordProduct.sql
@@ -0,0 +1,42 @@
+/****** Object:  StoredProcedure [dbo].[DeleteCordProduct]    Script Date: 11/26/2022 1:17:52 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	DeleteCordProduct
+-- [== History ==]
+-- <2022-08-20> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- 
+-- =============================================
+CREATE PROCEDURE [dbo].[DeleteCordProduct] (
+  @CordProductPkId int
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF EXISTS (SELECT TOP 1 * FROM CordProduct WHERE CordProductPkId = @CordProductPkId)
+        BEGIN
+            UPDATE CordProduct 
+               SET DeleteFlag = 1
+             WHERE @CordProductPkId = @CordProductPkId
+        END
+
+        -- Update Error Status/Message
+        SET @errNum = 0;
+        SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
