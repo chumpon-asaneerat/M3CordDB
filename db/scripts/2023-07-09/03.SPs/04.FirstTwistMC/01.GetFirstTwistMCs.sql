76bb9d2d49b0b81269532b4215e8bbf4e7489921
chumpon-asaneerat <chumponsenate@gmail.com>
2023-07-17 02:23:24 +0700

    Re-Implements FirstTwist #1 - Prepare RawMateiralSheet related sp.

---
+++ db/scripts/2023-07-09/03.SPs/04.FirstTwistMC/01.GetFirstTwistMCs.sql
@@ -0,0 +1,30 @@
+/****** Object:  StoredProcedure [dbo].[GetFirstTwistMCs]    Script Date: 11/27/2022 9:58:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetFirstTwistMCs
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetFirstTwistMCs NULL
+-- =============================================
+ALTER PROCEDURE [dbo].[GetFirstTwistMCs]
+AS
+BEGIN
+    SELECT MCCode
+         , ProcessName
+         , DeckPerCore
+         , StartCore
+         , EndCore
+      FROM FirstTwistMC
+     ORDER BY ProcessName, MCCode;
+END
+
+GO
---
+++ db/scripts/2023-07-09/03.SPs/04.FirstTwistMC/02.GetRawMaterialSheet.sql
@@ -0,0 +1,44 @@
+/****** Object:  StoredProcedure [dbo].[GetRawMaterialSheet]    Script Date: 11/27/2022 9:58:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetRawMaterialSheet
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetRawMaterialSheet NULL
+-- =============================================
+ALTER PROCEDURE [dbo].[GetRawMaterialSheet]
+(
+  @MCCode nvarchar(10)
+)
+AS
+BEGIN
+	SELECT RawMaterialSheetId
+	     , MCCode
+	     , CordProductPkId
+         , ProductLotNo
+         , CustomerCode
+         , CustomerName
+         , ItemYarn
+         , ItemCode
+         , Item400
+         , Color
+         , TargetQty
+         , ActualQty
+	     , DeleteFlag
+		 , FinishFlag
+      FROM RawMaterialSheetView
+     WHERE UPPER(LTRIM(RTRIM(MCCode))) = UPPER(LTRIM(RTRIM(@MCCode)))
+	   AND (DeleteFlag IS NULL OR DeleteFlag = 0)
+	   AND (FinishFlag IS NULL OR FinishFlag = 0)
+END
+
+GO
