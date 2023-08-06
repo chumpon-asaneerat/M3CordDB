03de605db244dcba4b02360a172de85505c7cad0
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-07 01:04:55 +0700

    Re-Implements FirstTwist #18

---
+++ db/scripts/2023-08-07/03.SPs/02.Warehouses/01.GetWarehouseYarns.sql
@@ -0,0 +1,64 @@
+/****** Object:  StoredProcedure [dbo].[GetWarehouseYarns]    Script Date: 8/7/2023 0:57:18 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetG4IssueYarns
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetWarehouseYarns NULL
+-- =============================================
+CREATE PROCEDURE [dbo].[GetWarehouseYarns]
+(
+  @PalletNo nvarchar(30) = NULL
+)
+AS
+BEGIN
+    SELECT I.G4IssueYarnPkId -- FROM G4IssueYarn
+         , I.RequestNo
+         , I.IssueDate
+         , I.IssueBy
+         , I.TraceNo
+         , I.PalletNo
+         , I.WeightQty
+         , I.ConeCH
+         , I.PalletType
+         , I.DeleteFlag
+         , I.FinishFlag
+         , I.WHReceiveFlag
+         , I.[Remark]
+         , I.UsedCH
+         , Y.G4YarnPkId -- FROM G4Yarn
+	     , Y.EntryDate
+		 , Y.LotNo
+		 , Y.ItemYarn
+		 , Y.Item400
+		 , Y.ReceiveDate
+		 , Y.ReceiveBy
+		 , Y.Verify
+         , Y.ExpiredDate
+		 --, Y.Packing
+		 --, Y.Clean
+		 --, Y.Tearing
+		 --, Y.Falldown
+      FROM G4IssueYarn I, G4Yarn Y
+     WHERE UPPER(LTRIM(RTRIM(I.PalletNo))) = UPPER(LTRIM(RTRIM(COALESCE(@PalletNo, I.PalletNo))))
+       AND I.PalletNo = Y.PalletNo
+       AND (I.DeleteFlag IS NULL OR I.DeleteFlag = 0)
+       AND (I.FinishFlag IS NULL OR I.FinishFlag = 0)
+       AND (I.WHReceiveFlag = 1)
+       AND (I.UsedCH IS NULL OR I.ConeCH > I.UsedCH)
+     ORDER BY I.IssueDate , I.PalletNo;
+
+END
+
+GO
