ec9c935ce5f77e7e8994c57e0906a25849abd13b
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-07 04:40:16 +0700

    Re-Implements FirstTwist #22

--- db/scripts/2023-08-07/01.Tables/11.YarnLoadSheet.sql
+++ db/scripts/2023-08-07/01.Tables/11.YarnLoadSheet.sql
@@ -9,8 +9,8 @@ CREATE TABLE [dbo].[YarnLoadSheet](
 	[YarnLoadSheetId] [int] IDENTITY(1,1) NOT NULL,
 	[CordProductPkId] [int] NOT NULL,
 	[MCCode] [nvarchar](10) NULL,
-	[DeleteFlag] [bit] NULL,
-	[FinishFlag] [bit] NULL,
+	[DeleteFlag] [bit] NULL DEFAULT 0,
+	[FinishFlag] [bit] NULL DEFAULT 0,
  CONSTRAINT [PK_YarnLoadSheet] PRIMARY KEY CLUSTERED 
 (
 	[YarnLoadSheetId] ASC
