b21afa707968cb6927a9352b1ddac06ab56c478d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-06 17:15:29 +0700

    Re-Implements FirstTwist #10

---
+++ db/scripts/2023-08-07/01.Tables/03.S1ConditionStd.sql
@@ -0,0 +1,37 @@
+/****** Object:  Table [dbo].[S1ConditionStd]    Script Date: 8/6/2023 16:59:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[S1ConditionStd](
+	[PkId] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerName] [nvarchar](100) NOT NULL,
+	[ItemCode] [nvarchar](30) NOT NULL,
+	[ProductName] [nvarchar](100) NOT NULL,
+	[ItemYarn] [nvarchar](30) NOT NULL,
+	[CordStructure1] [nvarchar](50) NOT NULL,
+	[CordStructure2] [nvarchar](50) NOT NULL,
+	[StdTwistingNumber] [nvarchar](50) NOT NULL,
+	[RingDiameter] [nvarchar](100) NULL,
+	[TwistChangeGearI] [int] NULL,
+	[TwistChangeGearJ] [int] NULL,
+	[TwistChangeGearK] [int] NULL,
+	[LifterChangGearA] [int] NULL,
+	[LifterChangGearB] [int] NULL,
+	[OuterDiameter] [int] NULL,
+	[TravellerNo] [nvarchar](100) NULL,
+	[CouterUnit] [decimal](18, 2) NULL,
+	[CouterUnitErrRange] [decimal](18, 2) NULL,
+	[CouterWg] [decimal](18, 2) NULL,
+	[CouterWgErrRange] [decimal](18, 2) NULL,
+	[CalcTwistingNumber] [decimal](18, 2) NULL,
+	[CalcTwistingNumberErrRange] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_S1ConditionStd] PRIMARY KEY CLUSTERED 
+(
+	[PkId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
