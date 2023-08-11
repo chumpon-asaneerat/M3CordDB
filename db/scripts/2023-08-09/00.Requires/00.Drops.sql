b0063645f1c583502b64f6904cbb63202bea7260
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-11 08:28:43 +0700

    Re-Implements FirstTwist #31

--- db/scripts/2023-08-09/00.Requires/00.Drops.sql
+++ db/scripts/2023-08-09/00.Requires/00.Drops.sql
@@ -1,12 +1,27 @@
 --DROP PROCEDURE SaveYarnLoadSheet
+--GO
 --DROP PROCEDURE GetYarnLoadSheets
+--GO
 --DROP PROCEDURE SaveYarnLoadSheetDoff
+--GO
+
+--DROP PROCEDURE SaveS1ConditionStd
+--GO
+--DROP PROCEDURE GetS1ConditionStds
+--GO
 
 --DROP PROCEDURE SaveCordProduct
+--GO
 --DROP PROCEDURE GetCordProducts
+--GO
 --DROP PROCEDURE DeleteCordProduct
+--GO
 
 --EXEC DropTable N'YarnLoadSheetItem'
+--GO
 --EXEC DropTable N'YarnLoadSheetDoff'
+--GO
 --EXEC DropTable N'YarnLoadSheet'
+--GO
 --EXEC DropTable N'CordProduct'
+--GO
