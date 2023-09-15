776db866ef88a97ff852b6e0d254f3ca42e3a243
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-15 14:13:56 +0700

    Re-Implements FirstTwist #69 - Prepare Script 2023-09-18 - Update SP GetRawMaterialSummaries

---
+++ db/scripts/2023-09-18/03.SPs/04.FirstTwistMC/05.GetRawMaterialSummaries.sql
@@ -0,0 +1,68 @@
+/****** Object:  StoredProcedure [dbo].[GetRawMaterialSummaries]    Script Date: 8/17/2023 8:42:14 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	GetRawMaterialSummaries
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC GetRawMaterialSummaries NULL
+-- =============================================
+ALTER PROCEDURE [dbo].[GetRawMaterialSummaries]
+(
+  @PCTwist1Id int
+)
+AS
+BEGIN
+		SELECT DISTINCT
+		       A.Twist1LoadId
+			 , A.PCTwist1Id
+		     , A.ProductionDate
+		     , A.ItemYarn
+			 , B.PalletNo
+			 , B.TraceNo
+			 , COUNT(TraceNo) AS InputCH
+			 , MIN(B.SPNo) AS SPNoMin
+			 , MAX(B.SPNo) AS SPNoMax
+		     , A.DoffNo
+             , A.Keiba
+             , A.Traverse
+             , A.Crossing
+             , A.ScrapeTube
+             , A.Form
+             , A.Stain
+			 , A.ShiftName
+             , A.Chief
+             , A.Remark
+        FROM Twist1LoadRecord A, Twist1LoadRecordItem B
+       WHERE B.Twist1LoadId = A.Twist1LoadId
+         AND A.PCTwist1Id =  @PCTwist1Id
+       GROUP BY A.Twist1LoadId
+              , A.PCTwist1Id
+		      , A.ProductionDate
+			  , A.ItemYarn
+	          , B.PalletNo
+			  , B.TraceNo
+		      , A.DoffNo
+              , A.Keiba
+              , A.Traverse
+              , A.Crossing
+              , A.ScrapeTube
+              , A.Form
+              , A.Stain
+			  , A.ShiftName
+              , A.Chief
+              , A.Remark
+
+END
+
+GO
