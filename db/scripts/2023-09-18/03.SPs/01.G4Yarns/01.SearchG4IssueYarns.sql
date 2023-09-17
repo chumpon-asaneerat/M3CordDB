5b2250f938be3d69160e252609e41554c141dd6d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-17 23:21:47 +0700

    Re-Implements FirstTwist #72

---
+++ db/scripts/2023-09-18/03.SPs/01.G4Yarns/01.SearchG4IssueYarns.sql
@@ -0,0 +1,69 @@
+/****** Object:  StoredProcedure [dbo].[SearchG4IssueYarns]    Script Date: 14/9/2566 13:34:02 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SearchG4IssueYarns
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+-- <2023-09-14> :
+--	- Add parameter Request No
+--	- Comment parameter @IssueDate  
+--
+-- [== Example ==]
+--
+-- EXEC SearchG4IssueYarns NULL
+-- =============================================
+ALTER PROCEDURE [dbo].[SearchG4IssueYarns]
+(
+  @ItemYarn nvarchar(30) = NULL
+, @RequestNo nvarchar(30) = NULL
+)
+AS
+BEGIN
+    SELECT I.G4IssueYarnPkId -- FROM G4IssueYarn
+         , I.RequestNo
+         , I.IssueDate
+         , I.IssueBy
+         , I.TraceNo
+         , I.PalletNo
+         , I.WeightQty
+         , I.ConeCH
+         , I.PalletType
+         , I.DeleteFlag
+         , I.FinishFlag
+         , I.WHReceiveFlag
+         , I.WHReceiveDate
+         , I.[Remark]
+         , I.UsedCH
+         , Y.G4YarnPkId -- FROM G4Yarn
+	     , Y.EntryDate
+		 , Y.LotNo
+		 , Y.ItemYarn
+		 , Y.Item400
+		 , Y.ReceiveDate
+		 , Y.ReceiveBy
+         , Y.ExpiredDate
+		 , Y.Verify
+		 --, Y.Packing
+		 --, Y.Clean
+		 --, Y.Tearing
+		 --, Y.Falldown
+      FROM G4IssueYarn I, G4Yarn Y
+     WHERE UPPER(LTRIM(RTRIM(Y.ItemYarn))) = UPPER(LTRIM(RTRIM(COALESCE(@ItemYarn, Y.ItemYarn))))
+       --AND UPPER(LTRIM(RTRIM(Y.Item400))) = UPPER(LTRIM(RTRIM(COALESCE(@Item400, Y.Item400))))
+       AND I.PalletNo = Y.PalletNo
+       AND (I.DeleteFlag IS NULL OR I.DeleteFlag = 0)
+       AND (I.WHReceiveFlag IS NULL OR I.WHReceiveFlag = 0)
+       AND (I.UsedCH IS NULL OR I.ConeCH > I.UsedCH)
+	   AND I.RequestNo = COALESCE(@RequestNo, I.RequestNo)
+     ORDER BY I.IssueDate , I.PalletNo;
+
+END
+
+GO
+
