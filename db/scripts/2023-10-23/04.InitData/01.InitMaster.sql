f9605f334deedc38313e20940946e0c752b99eb3
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-25 13:59:21 +0700

    Re-Implements FirstTwist #155 - DIP #15

--- db/scripts/2023-10-23/04.InitData/01.InitMaster.sql
+++ db/scripts/2023-10-23/04.InitData/01.InitMaster.sql
@@ -1,4 +1,5 @@
-INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-7', N'S-7', 1, 130);
-INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-8', N'S-8-1', 1, 130);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-7-1', N'S-7', 1, 130);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-7-2', N'S-7', 1, 150);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-8', N'S-8', 1, 130);
 INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-9', N'S-9', 1, 130);
 GO
