80301096cd640575e70d7b22d59d543658632b3b
chumpon-asaneerat <chumponsenate@gmail.com>
2023-11-03 05:28:50 +0700

    Re-Implements FirstTwist #177 - DIP #37   - Init data

---
+++ db/scripts/2023-11-03/04.InitData/01.InitPalletCodeForDIP.sql
@@ -0,0 +1,5 @@
+/*
+INSERT INTO PalletCode(MCCode, [Year], LastId) VALUES('S-9-1', 2023, 0);
+INSERT INTO PalletCode(MCCode, [Year], LastId) VALUES('S-9-2', 2023, 0);
+GO
+*/
---
+++ db/scripts/2023-11-03/04.InitData/02.InitDIPMC.sql
@@ -0,0 +1,10 @@
+DELETE FROM DIPMC;
+GO
+
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-7-1', N'S-7', 1, 130);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-7-2', N'S-7', 1, 150);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-8-1', N'S-8', 1, 130);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-8-2', N'S-8', 1, 150);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-9-1', N'S-9', 1, 130);
+INSERT INTO DIPMC(MCCode, ProcessName, StartCore, EndCore) VALUES(N'S-9-2', N'S-9', 1, 150);
+GO
