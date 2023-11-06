05d86ec3d04f23ee79ccc9f4657de33a3d50b14d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-11-07 02:03:36 +0700

    Re-Implements FirstTwist #180 - DIP #40

---
+++ db/scripts/2023-11-03/01.Tables/02.DIP/01.DIPPalletSlip.sql
@@ -0,0 +1,26 @@
+/****** Object:  Table [dbo].[DIPPalletSlip]    Script Date: 11/7/2023 2:02:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DIPPalletSlip](
+	[PalletId] [int] IDENTITY(1,1) NOT NULL,
+	[DIPPCId] [int] NULL,
+	[PalletCode] [nvarchar](30) NULL,
+	[CreateDate] [datetime] NULL,
+	[TwistNo] [nvarchar](100) NULL,
+	[Counter] [int] NULL,
+	[UserName] [nvarchar](100) NULL,
+	[PalletStatus] [int] NULL,
+ CONSTRAINT [PK_DIPPalletSlip] PRIMARY KEY CLUSTERED 
+(
+	[PalletId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[DIPPalletSlip] ADD  CONSTRAINT [DF_DIPPalletSlip_CreateDate]  DEFAULT (getdate()) FOR [CreateDate]
+GO
