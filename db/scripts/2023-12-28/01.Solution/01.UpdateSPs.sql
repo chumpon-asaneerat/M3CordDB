b1299dbc776851dfe43f95e8d7060d7f35e63395
chumpon-asaneerat <chumponsenate@gmail.com>
2023-12-30 04:32:20 +0700

    Re-Implements #233 - Update 2023-12-28 script.

--- db/scripts/2023-12-28/01.Solution/01.UpdateSPs.sql
+++ db/scripts/2023-12-28/01.Solution/01.UpdateSPs.sql
@@ -30,15 +30,12 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 ALTER PROCEDURE [dbo].[ChGetSolutionLotDetail]
 (
   @solutionlot nvarchar(30) = NULL
 )
 AS
 BEGIN
-
-
 	select s.* , isnull(c.ChemicalName, s.ChemicalNo) AS ChemicalName, r.ChemicalType
 	from  [dbo].[SolutionLotDetail] s 
 	left join [dbo].[ChemicalMaster] c on s.ChemicalNo = c.ChemicalNo
@@ -49,6 +46,70 @@ BEGIN
 	and s.[SolutionLot] = @solutionlot
 	order by s.[RecipeOrder] , s.[MixOrder] asc;
 
+END
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[ChSaveSolutionLotDetail]    Script Date: 28/12/2566 2:21:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[ChSaveSolutionLotDetail] (
+  @solutionlot nvarchar(10)
+, @solutionid int
+, @recipeorder int
+, @recipe nvarchar(15)
+, @mixorder int
+, @chemicalno nvarchar(5)
+, @weightcal decimal(8,4)
+, @weightactual decimal(8,4)
+, @weightmc nvarchar(50)
+, @weightdate datetime
+, @weightby int
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF EXISTS (SELECT TOP 1 * FROM  [dbo].[SolutionLotDetail]
+                    WHERE [SolutionLot] = @solutionlot
+					and [Recipe] = @recipe
+					and [ChemicalNo] = @chemicalno)
+        BEGIN
+            UPDATE [dbo].[SolutionLotDetail] 
+               SET  [WeightCal]= COALESCE(@weightcal, [WeightCal])
+                 , [WeightActual] = COALESCE(@weightactual, [WeightActual])
+                 , [WeightMc] = COALESCE(@weightmc, [WeightMc])
+                 , [WeightDate] = COALESCE(@weightdate, [WeightDate])
+                 , [WeightBy] = COALESCE(@weightby, [WeightBy])
+                 
+             WHERE [SolutionLot] = @solutionlot
+					and [Recipe] = @recipe
+					and [ChemicalNo] = @chemicalno
+        END
+        ELSE
+        BEGIN
+			INSERT INTO [dbo].[SolutionLotDetail]
+		    (
+                [SolutionLot] ,[SolutionID] ,[RecipeOrder] ,[Recipe] ,[MixOrder] ,[ChemicalNo]
+				, [WeightCal], [WeightActual],[WeightMc], [WeightDate], [WeightBy])
+                
+			VALUES
+			( @solutionlot,@solutionid, @recipeorder, @recipe,@mixorder,@chemicalno
+			,@weightcal,@weightactual,@weightmc,@weightdate,@weightby ) ;
+        END
+
+        -- Update Error Status/Message
+        SET @errNum = 0;
+        SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
 END
 
 GO
