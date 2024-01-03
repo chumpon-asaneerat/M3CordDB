e631a86af4b9e8f248f77cf5ccd10d587bd602d3
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-03 12:17:02 +0700

    Re-Implements #236 - Generate 2024-01-02 script.

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('bundle-js', () => {
 
 gulp.task('merge-sql-scripts', () => {
     let task = new GulpFileMerge();
-    let sDate = '2023-12-28';
+    let sDate = '2024-01-02';
     let rootPath = path.join(__dirname, 'db/scripts/' + sDate);
     task.opts = {
         src: [
