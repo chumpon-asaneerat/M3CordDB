ff1a86f9147a12e183c827a27c68631a0e4fc6a2
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-08 16:14:59 +0700

    Re-Implements #239 - Update gulpfile - 2024-01-08

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('bundle-js', () => {
 
 gulp.task('merge-sql-scripts', () => {
     let task = new GulpFileMerge();
-    let sDate = '2024-01-02';
+    let sDate = '2024-01-08';
     let rootPath = path.join(__dirname, 'db/scripts/' + sDate);
     task.opts = {
         src: [
