59ee82d31f6d9f424127fcb292a221f464974bbb
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-12 08:07:20 +0700

    Re-Implements #257 - S8BeforeCondition redesign #7

--- db/scripts/2024-01-10/01.Tables/02.DIPTimeTable.sql
+++ db/scripts/2024-01-10/01.Tables/02.DIPTimeTable.sql
@@ -1,4 +1,4 @@
-/****** Object:  Table [dbo].[DIPTimeTable]    Script Date: 1/11/2024 1:35:19 ******/
+/****** Object:  Table [dbo].[DIPTimeTable]    Script Date: 1/12/2024 8:01:53 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,30 +9,46 @@ CREATE TABLE [dbo].[DIPTimeTable](
 	[DIPTimeTableId] [int] IDENTITY(1,1) NOT NULL,
 	[DIPPCId] [int] NOT NULL,
 	[ProductCode] [nvarchar](30) NULL,
-	[RowType] int NULL,
+	[RowType] [int] NULL,
 	[PeriodTime] [datetime] NULL,
 	[S7BobbinSC] [bit] NULL,
 	[S7Bobbin] [bit] NULL,
 	[S8CoolingWaterSystemBath1SC] [bit] NULL,
 	[S8CoolingWaterSystemBath1] [decimal](18, 3) NULL,
+	[S8CoolingWaterSystemBath1Err] [decimal](18, 3) NULL,
+	[S8CoolingWaterSystemBath1Value] [decimal](18, 3) NULL,
 	[S8CoolingWaterSystemBath2SC] [bit] NULL,
 	[S8CoolingWaterSystemBath2] [decimal](18, 3) NULL,
+	[S8CoolingWaterSystemBath2Err] [decimal](18, 3) NULL,
+	[S8CoolingWaterSystemBath2Value] [decimal](18, 3) NULL,
 	[S8ChemicalWorkSC] [bit] NULL,
 	[S8ChemicalWork] [bit] NULL,
 	[S8ChemicalFilterSC] [bit] NULL,
 	[S8ChemicalFilter] [bit] NULL,
 	[S8SpeedSC] [bit] NULL,
 	[S8Speed] [decimal](18, 3) NULL,
+	[S8SpeedErr] [decimal](18, 3) NULL,
+	[S8SpeedValue] [decimal](18, 3) NULL,
 	[S8StretchDSC] [bit] NULL,
 	[S8StretchD] [decimal](18, 3) NULL,
+	[S8StretchDErr] [decimal](18, 3) NULL,
+	[S8StretchDValue] [decimal](18, 3) NULL,
 	[S8StretchHSC] [bit] NULL,
 	[S8StretchH] [decimal](18, 3) NULL,
+	[S8StretchHErr] [decimal](18, 3) NULL,
+	[S8StretchHValue] [decimal](18, 3) NULL,
 	[S8StretchNSC] [bit] NULL,
 	[S8StretchN] [decimal](18, 3) NULL,
+	[S8StretchNErr] [decimal](18, 3) NULL,
+	[S8StretchNValue] [decimal](18, 3) NULL,
 	[S8TempDSC] [bit] NULL,
 	[S8TempD] [decimal](18, 3) NULL,
+	[S8TempDErr] [decimal](18, 3) NULL,
+	[S8TempDValue] [decimal](18, 3) NULL,
 	[S8TempHNSC] [bit] NULL,
 	[S8TempHN] [decimal](18, 3) NULL,
+	[S8TempHNErr] [decimal](18, 3) NULL,
+	[S8TempHNValue] [decimal](18, 3) NULL,
 	[S9GlideStatusSC] [bit] NULL,
 	[S9GlideStatus] [bit] NULL,
 	[Remark] [nvarchar](200) NULL,
@@ -43,4 +59,3 @@ CREATE TABLE [dbo].[DIPTimeTable](
 ) ON [PRIMARY]
 
 GO
-
---
+++ db/scripts/2024-01-10/01.Tables/04.S8BeforeCondition.sql
@@ -0,0 +1,79 @@
+/****** Object:  Table [dbo].[S8BeforeCondition]    Script Date: 1/12/2024 8:02:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[S8BeforeCondition](
+	[S8BeforeId] [int] IDENTITY(1,1) NOT NULL,
+	[DIPPCId] [int] NOT NULL,
+	[ProductCode] [nvarchar](30) NULL,
+	[LotNo] [nvarchar](50) NULL,
+	[SolutionNameBath1SC] [bit] NULL,
+	[SolutionNameBath1] [nvarchar](50) NULL,
+	[SolutionNameBath2SC] [bit] NULL,
+	[SolutionNameBath2] [nvarchar](50) NULL,
+	[TempJacketDrumBath1SC] [bit] NULL,
+	[TempJacketDrumBath1Min] [decimal](18, 3) NULL,
+	[TempJacketDrumBath1Max] [decimal](18, 3) NULL,
+	[TempJacketDrumBath1] [decimal](18, 3) NULL,
+	[TempJacketDrumBath2SC] [bit] NULL,
+	[TempJacketDrumBath2Min] [decimal](18, 3) NULL,
+	[TempJacketDrumBath2Max] [decimal](18, 3) NULL,
+	[TempJacketDrumBath2] [decimal](18, 3) NULL,
+	[TempChemicalBath1SC] [bit] NULL,
+	[TempChemicalBath1Min] [decimal](18, 3) NULL,
+	[TempChemicalBath1Max] [decimal](18, 3) NULL,
+	[TempChemicalBath1] [decimal](18, 3) NULL,
+	[TempChemicalBath2SC] [bit] NULL,
+	[TempChemicalBath2Min] [decimal](18, 3) NULL,
+	[TempChemicalBath2Max] [decimal](18, 3) NULL,
+	[TempChemicalBath2] [decimal](18, 3) NULL,
+	[StretchDSC] [bit] NULL,
+	[StretchD] [decimal](18, 3) NULL,
+	[StretchDErr] [decimal](18, 3) NULL,
+	[StretchDValue] [decimal](18, 3) NULL,
+	[StretchHSC] [bit] NULL,
+	[StretchH] [decimal](18, 3) NULL,
+	[StretchHErr] [decimal](18, 3) NULL,
+	[StretchHValue] [decimal](18, 3) NULL,
+	[StretchNSC] [bit] NULL,
+	[StretchN] [decimal](18, 3) NULL,
+	[StretchNErr] [decimal](18, 3) NULL,
+	[StretchNValue] [decimal](18, 3) NULL,
+	[TempDSC] [bit] NULL,
+	[TempD] [decimal](18, 3) NULL,
+	[TempDErr] [decimal](18, 3) NULL,
+	[TempDValue] [decimal](18, 3) NULL,
+	[TempHNSC] [bit] NULL,
+	[TempHN] [decimal](18, 3) NULL,
+	[TempHNErr] [decimal](18, 3) NULL,
+	[TempHNValue] [decimal](18, 3) NULL,
+	[SpeedSC] [bit] NULL,
+	[Speed] [decimal](18, 3) NULL,
+	[SpeedErr] [decimal](18, 3) NULL,
+	[SpeedValue] [decimal](18, 3) NULL,
+	[ExhaustFanDryerSC] [bit] NULL,
+	[ExhaustFanDryer] [decimal](18, 3) NULL,
+	[ExhaustFanDryerValue] [decimal](18, 3) NULL,
+	[ExhaustFanHNSC] [bit] NULL,
+	[ExhaustFanHN] [decimal](18, 3) NULL,
+	[ExhaustFanHNValue] [decimal](18, 3) NULL,
+	[CleanBath1SC] [bit] NULL,
+	[CleanBath1] [bit] NULL,
+	[CleanBath2SC] [bit] NULL,
+	[CleanBath2] [bit] NULL,
+	[CleanFrontSC] [bit] NULL,
+	[CleanFront] [bit] NULL,
+	[CamboxSC] [bit] NULL,
+	[Cambox] [bit] NULL,
+	[CheckBy] [nvarchar](100) NULL,
+	[VerifyBy] [nvarchar](100) NULL,
+ CONSTRAINT [PK_S8BeforeCondition] PRIMARY KEY CLUSTERED 
+(
+	[S8BeforeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
