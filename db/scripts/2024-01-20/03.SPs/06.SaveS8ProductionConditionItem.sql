3a01864a31b5e718df2497b7f85b1e7c86bd0545
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-20 13:13:18 +0700

    Re-Implements #292 - Implements S8ProductionCondition #9

---
+++ db/scripts/2024-01-20/03.SPs/06.SaveS8ProductionConditionItem.sql
@@ -0,0 +1,323 @@
+/****** Object:  StoredProcedure [dbo].[SaveS8ProductionConditionItem]    Script Date: 1/20/2024 13:00:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[SaveS8ProductionConditionItem] (
+  @ProductCode nvarchar (30) 
+, @LotNo nvarchar (30) 
+, @DoffingDate datetime  
+, @DoffingNo int  
+
+, @StretchDSC bit  
+, @StretchD decimal (18, 3) 
+, @StretchDErr decimal (18, 3) 
+, @StretchDValue decimal (18, 3) 
+
+, @StretchHSC bit  
+, @StretchH decimal (18, 3) 
+, @StretchHErr decimal (18, 3) 
+, @StretchHValue decimal (18, 3) 
+
+, @StretchNSC bit  
+, @StretchN decimal (18, 3) 
+, @StretchNErr decimal (18, 3) 
+, @StretchNValue decimal (18, 3) 
+
+, @TempDSC bit  
+, @TempD decimal (18, 3) 
+, @TempDErr decimal (18, 3) 
+, @TempDValue decimal (18, 3) 
+
+, @TempHNSC bit  
+, @TempHN decimal (18, 3) 
+, @TempHNErr decimal (18, 3) 
+, @TempHNValue decimal (18, 3) 
+
+, @SpeedSC bit  
+, @Speed decimal (18, 3) 
+, @SpeedErr decimal (18, 3) 
+, @SpeedValue decimal (18, 3) 
+
+, @TreatSC bit  
+, @Treat decimal (18, 3) 
+, @TreatValue decimal (18, 3) 
+
+, @DoffingLengthSC bit  
+, @DoffingLength decimal (18, 3) 
+, @DoffingLengthValue decimal (18, 3) 
+
+, @WeightSC bit  
+, @Weight decimal (18, 3) 
+, @WeightValue decimal (18, 3) 
+
+, @SpindleSC bit  
+, @Spindle decimal (18, 3) 
+, @SpindleValue decimal (18, 3) 
+
+, @ProductionGoodSC bit  
+, @ProductionGood decimal (18, 3) 
+, @ProductionGoodValue decimal (18, 3) 
+
+, @ProductionTotalSC bit  
+, @ProductionTotal decimal (18, 3) 
+, @ProductionTotalValue decimal (18, 3) 
+
+, @ProductionCut decimal (18, 3) 
+, @PositionCordCutCreel decimal (18, 3) 
+, @PositionCordCutCS decimal (18, 3) 
+, @PositionCordCutWinder decimal (18, 3) 
+, @PositionCordCutWasteYarn decimal (18, 3) 
+
+, @CheckTimeStart datetime  
+, @CheckTimeFinish datetime  
+, @CheckTimeRecord datetime  
+, @Opertor nvarchar (100) 
+, @Leader nvarchar (100) 
+, @errNum as int = 0 out
+, @errMsg as nvarchar(MAX) = N'' out)
+AS
+BEGIN
+	BEGIN TRY
+        IF EXISTS (SELECT TOP 1 * 
+                     FROM S8ProductionConditionItem 
+                    WHERE ProductCode = @ProductCode 
+                      AND LotNo = @LotNo 
+                      AND DoffingNo = @DoffingNo)
+        BEGIN
+            UPDATE S8ProductionConditionItem 
+               SET DoffingDate = @DoffingDate  
+				 , DoffingNo = @DoffingNo 
+
+                 , StretchDSC = @StretchDSC  
+				 , StretchD = @StretchD 
+				 , StretchDErr = @StretchDErr 
+				 , StretchDValue = @StretchDValue
+
+				 , StretchHSC = @StretchHSC  
+				 , StretchH = @StretchH 
+				 , StretchHErr = @StretchHErr 
+				 , StretchHValue = @StretchHValue 
+
+				 , StretchNSC = @StretchNSC  
+				 , StretchN = @StretchN 
+				 , StretchNErr = @StretchNErr 
+				 , StretchNValue = @StretchNValue 
+
+				 , TempDSC = @TempDSC  
+				 , TempD = @TempD 
+				 , TempDErr = @TempDErr 
+				 , TempDValue = @TempDValue 
+
+				 , TempHNSC = @TempHNSC  
+				 , TempHN = @TempHN 
+				 , TempHNErr = @TempHNErr 
+				 , TempHNValue = @TempHNValue 
+
+				 , SpeedSC = @SpeedSC  
+				 , Speed = @Speed 
+				 , SpeedErr = @SpeedErr 
+				 , SpeedValue = @SpeedValue 
+
+				 , TreatSC = @TreatSC  
+				 , Treat = @Treat 
+				 , TreatValue = @TreatValue 
+
+				 , DoffingLengthSC = @DoffingLengthSC  
+				 , DoffingLength = @DoffingLength 
+				 , DoffingLengthValue = @DoffingLengthValue 
+
+				 , WeightSC = @WeightSC  
+				 , [Weight] = @Weight 
+				 , WeightValue = @WeightValue 
+
+				 , SpindleSC = @SpindleSC  
+				 , Spindle = @Spindle 
+				 , SpindleValue = @SpindleValue 
+
+				 , ProductionGoodSC = @ProductionGoodSC  
+				 , ProductionGood = @ProductionGood 
+				 , ProductionGoodValue = @ProductionGoodValue 
+
+				 , ProductionTotalSC = @ProductionTotalSC  
+				 , ProductionTotal = @ProductionTotal  
+				 , ProductionTotalValue = @ProductionTotalValue 
+
+				 , ProductionCut = @ProductionCut 
+				 , PositionCordCutCreel = @PositionCordCutCreel 
+				 , PositionCordCutCS = @PositionCordCutCS 
+				 , PositionCordCutWinder = @PositionCordCutWinder 
+				 , PositionCordCutWasteYarn = @PositionCordCutWasteYarn 
+				 , CheckTimeStart = @CheckTimeStart  
+				 , CheckTimeFinish = @CheckTimeFinish  
+				 , CheckTimeRecord = @CheckTimeRecord  
+				 , Opertor = @Opertor 
+				 , Leader = @Leader   
+             WHERE ProductCode = @ProductCode  
+               AND LotNo = @LotNo 
+               AND DoffingNo = @DoffingNo
+        END
+        ELSE
+        BEGIN
+			INSERT INTO S8ProductionConditionItem
+		    (
+				  ProductCode 
+				, LotNo 
+				, DoffingDate 
+				, DoffingNo
+
+				, StretchDSC  
+				, StretchD 
+				, StretchDErr 
+				, StretchDValue 
+
+				, StretchHSC  
+				, StretchH 
+				, StretchHErr 
+				, StretchHValue 
+
+				, StretchNSC  
+				, StretchN 
+				, StretchNErr 
+				, StretchNValue 
+
+				, TempDSC  
+				, TempD 
+				, TempDErr 
+				, TempDValue 
+
+				, TempHNSC  
+				, TempHN 
+				, TempHNErr 
+				, TempHNValue 
+				
+				, SpeedSC  
+				, Speed 
+				, SpeedErr 
+                , SpeedValue 
+
+				, TreatSC  
+				, Treat 
+				, TreatValue 
+
+				, DoffingLengthSC  
+				, DoffingLength 
+				, DoffingLengthValue 
+
+				, WeightSC  
+				, [Weight] 
+				, WeightValue 
+
+				, SpindleSC  
+				, Spindle 
+				, SpindleValue 
+
+				, ProductionGoodSC  
+				, ProductionGood 
+				, ProductionGoodValue 
+
+				, ProductionTotalSC  
+				, ProductionTotal 
+				, ProductionTotalValue 
+
+				, ProductionCut 
+				, PositionCordCutCreel 
+				, PositionCordCutCS 
+				, PositionCordCutWinder 
+				, PositionCordCutWasteYarn 
+
+				, CheckTimeStart 
+				, CheckTimeFinish 
+				, CheckTimeRecord 
+
+				, Opertor 
+		        , Leader 
+			)
+			VALUES
+			(
+                  @ProductCode 
+				, @LotNo 
+				, @DoffingDate 
+				, @DoffingNo
+
+			    , @StretchDSC  
+			    , @StretchD 
+			    , @StretchDErr 
+				, @StretchDValue 
+
+			    , @StretchHSC  
+			    , @StretchH 
+			    , @StretchHErr 
+				, @StretchHValue 
+
+			    , @StretchNSC  
+			    , @StretchN 
+			    , @StretchNErr 
+				, @StretchNValue 
+
+			    , @TempDSC  
+			    , @TempD 
+			    , @TempDErr 
+				, @TempDValue 
+
+			    , @TempHNSC  
+			    , @TempHN 
+			    , @TempHNErr 
+				, @TempHNValue 
+
+			    , @SpeedSC  
+			    , @Speed 
+			    , @SpeedErr 
+				, @SpeedValue 
+
+			    , @TreatSC  
+			    , @Treat 
+				, @TreatValue 
+
+			    , @DoffingLengthSC  
+			    , @DoffingLength 
+				, @DoffingLengthValue 
+
+			    , @WeightSC  
+			    , @Weight 
+				, @WeightValue 
+
+			    , @SpindleSC  
+			    , @Spindle 
+				, @SpindleValue 
+
+			    , @ProductionGoodSC  
+			    , @ProductionGood 
+				, @ProductionGoodValue 
+
+			    , @ProductionTotalSC  
+			    , @ProductionTotal 
+				, @ProductionTotalValue 
+
+				, @ProductionCut 
+				, @PositionCordCutCreel 
+				, @PositionCordCutCS 
+				, @PositionCordCutWinder 
+				, @PositionCordCutWasteYarn 
+
+				, @CheckTimeStart  
+				, @CheckTimeFinish  
+				, @CheckTimeRecord  
+
+				, @Opertor 
+				, @Leader 
+			);
+        END
+
+        -- Update Error Status/Message
+        SET @errNum = 0;
+        SET @errMsg = 'Success';
+	END TRY
+	BEGIN CATCH
+		SET @errNum = ERROR_NUMBER();
+		SET @errMsg = ERROR_MESSAGE();
+	END CATCH
+END
+
+GO
