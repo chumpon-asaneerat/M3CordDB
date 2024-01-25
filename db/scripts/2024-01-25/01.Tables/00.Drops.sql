562f2a22313939d45f65041fb0c7de1cdfaa8943
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-25 15:27:35 +0700

    Re-Implements #299 - Prepare 2024-01-25 script.

---
+++ db/scripts/2024-01-25/01.Tables/00.Drops.sql
@@ -0,0 +1,26 @@
+DROP PROCEDURE GetProductionRecordDetail
+GO
+
+DROP PROCEDURE GetProductionRecord
+GO
+
+DROP PROCEDURE GetProductionRecordStd
+GO
+
+DROP PROCEDURE SaveProductionRecordDetail
+GO
+
+DROP PROCEDURE SaveProductionRecord
+GO
+
+DROP PROCEDURE SaveProductionRecordStd
+GO
+
+EXEC DROPTABLE 'ProductionRecordDetail'
+GO
+
+EXEC DROPTABLE 'ProductionRecord'
+GO
+
+EXEC DROPTABLE 'ProductionRecordStd'
+GO
