0c36ae29d28c8b482a9442f551704553f16f527f
chumpon-asaneerat <chumponsenate@gmail.com>
2024-02-16 14:53:25 +0700

    Re-Implements #315 - Implements Wet Pickup #5

---
+++ db/scripts/2024-02-12/01.Tables/03.S8WetPickUpItem.sql
@@ -0,0 +1,26 @@
+/****** Object:  Table [dbo].[S8WetPickUpItem]    Script Date: 2/16/2024 14:51:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[S8WetPickUpItem](
+	[ProductCode] [nvarchar](30) NULL,
+	[LotNo] [nvarchar](30) NULL,
+	[DoffingDate] [datetime] NULL,
+	[DoffNo] [int] NULL,
+	[TwistNo] [int] NULL,
+	[RowType] [int] NULL,
+	[NipPressure] [nvarchar](50) NULL,
+	[WPU] [decimal](18, 3) NULL,
+	[WPUErr] [decimal](18, 3) NULL,
+	[FirstAmt] [decimal](18, 3) NULL,
+	[UseAmt] [decimal](18, 3) NULL,
+	[RestAmt] [decimal](18, 3) NULL,
+	[ThrowAmt] [decimal](18, 3) NULL,
+	[Operator] [nvarchar](100) NULL,
+	[UpdateDate] [datetime] NULL
+) ON [PRIMARY]
+
+GO
