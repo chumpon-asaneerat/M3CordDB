b1c3c5b9fdfa701a73a885b0cd4bc8bd2f70fcd8
chumpon-asaneerat <chumponsenate@gmail.com>
2024-03-12 21:07:10 +0700

    Re-Implements #333 - Create Script for 2024-03-12   - Add SP SearchG4YarnStockByTraceNo

---
+++ db/scripts/2024-03-12/03.SPs/01.SearchG4YarnStockByTraceNo.sql
@@ -0,0 +1,35 @@
+/****** Object:  StoredProcedure [dbo].[SearchG4YarnStockByTraceNo]    Script Date: 3/12/2024 16:52:53 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author: Chumpon Asaneerat
+-- Description:	SearchG4YarnStockByTraceNo
+-- [== History ==]
+-- <2023-04-26> :
+--	- Stored Procedure Created.
+--
+-- [== Example ==]
+--
+-- EXEC SearchG4YarnStockByTraceNo NULL, NULL
+-- =============================================
+CREATE PROCEDURE [dbo].[SearchG4YarnStockByTraceNo]
+(
+  @TraceNo nvarchar(30) = NULL
+, @ItemYarn nvarchar(30) = NULL
+)
+AS
+BEGIN
+	SELECT *
+	  FROM G4YarnView
+	 WHERE UPPER(LTRIM(RTRIM(TraceNo))) = UPPER(LTRIM(RTRIM(COALESCE(@TraceNo, TraceNo))))
+       AND UPPER(LTRIM(RTRIM(ItemYarn))) = UPPER(LTRIM(RTRIM(COALESCE(@ItemYarn, ItemYarn))))
+       AND (DeleteFlag IS NULL OR DeleteFlag = 0)
+       AND (FinishFlag IS NULL OR FinishFlag = 0)
+       AND (ReceiveDate IS NOT NULL)
+END
+
+GO
+
